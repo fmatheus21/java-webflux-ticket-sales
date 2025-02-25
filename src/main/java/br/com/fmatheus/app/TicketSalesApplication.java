5ef5fbf08699ddbca7364fc07a5d4eae1bdaa268
Fernando Matheus <fernando.matheuss@hotmail.com>
2025-02-24 22:15:00 -0300

    first commit

---
+++ src/main/java/br/com/fmatheus/app/TicketSalesApplication.java
@@ -0,0 +1,13 @@
+package br.com.fmatheus.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSalesApplication.class, args);
+	}
+
+}
